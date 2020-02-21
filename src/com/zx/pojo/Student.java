ddcc079835f78af854b0686953a31af3c782b04b
ZexiGong <gong.ze@husky.neu.edu>
2020-02-21 10:36:11 +0800

    create student class

---
+++ src/com/zx/pojo/Student.java
@@ -0,0 +1,125 @@
+package com.zx.pojo;
+
+public class Student {
+	private String studentNumber;
+	private String name;
+	private int age;
+	private String sex;
+	private String phone;
+	private String city;
+	private String info;
+	public String getStudentNumber() {
+		return studentNumber;
+	}
+	public void setStudentNumber(String studentNumber) {
+		this.studentNumber = studentNumber;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getSex() {
+		return sex;
+	}
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getInfo() {
+		return info;
+	}
+	public void setInfo(String info) {
+		this.info = info;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + age;
+		result = prime * result + ((city == null) ? 0 : city.hashCode());
+		result = prime * result + ((info == null) ? 0 : info.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + ((phone == null) ? 0 : phone.hashCode());
+		result = prime * result + ((sex == null) ? 0 : sex.hashCode());
+		result = prime * result + ((studentNumber == null) ? 0 : studentNumber.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Student other = (Student) obj;
+		if (age != other.age)
+			return false;
+		if (city == null) {
+			if (other.city != null)
+				return false;
+		} else if (!city.equals(other.city))
+			return false;
+		if (info == null) {
+			if (other.info != null)
+				return false;
+		} else if (!info.equals(other.info))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (phone == null) {
+			if (other.phone != null)
+				return false;
+		} else if (!phone.equals(other.phone))
+			return false;
+		if (sex == null) {
+			if (other.sex != null)
+				return false;
+		} else if (!sex.equals(other.sex))
+			return false;
+		if (studentNumber == null) {
+			if (other.studentNumber != null)
+				return false;
+		} else if (!studentNumber.equals(other.studentNumber))
+			return false;
+		return true;
+	}
+	@Override
+	public String toString() {
+		return "Student [studentNumber=" + studentNumber + ", name=" + name + ", age=" + age + ", sex=" + sex
+				+ ", phone=" + phone + ", city=" + city + ", info=" + info + "]";
+	}
+	public Student(String studentNumber, String name, int age, String sex, String phone, String city, String info) {
+		super();
+		this.studentNumber = studentNumber;
+		this.name = name;
+		this.age = age;
+		this.sex = sex;
+		this.phone = phone;
+		this.city = city;
+		this.info = info;
+	}
+	
+}
